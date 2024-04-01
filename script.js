3c36933ec64f3b88691f65b1c357e07a33cf2731
Guilherme Paiva <castroluana687@gmail.com>
2024-04-01 11:28:06 -0300

    Quarto commit do JS

--- script.js
+++ script.js
@@ -37,7 +37,16 @@ function receberPorcentagem(evento) {
             botao.classList.add("botao-ativo")
         }
     })
-     
-    porcentagem = parseFloat(evento.target.value)/100
+    
+    if(evento.target.value !== "") {
+        porcentagem = parseFloat(evento.target.value)/100
+    }else{
+        porcentagem = 0
+    }
 
+    console.log(porcentagem)
+    
 }
+
+const gorjetaInput = document.querySelector("#outra")
+gorjetaInput.addEventListener("input", receberPorcentagem)
